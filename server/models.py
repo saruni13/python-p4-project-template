4cd2e57c789cf3cb4b01883140587f4b309ae8c7
Adida <adida.barack@student.moringaschool.com>
2024-07-10 12:36:25 +0300

    suppliers model added

--- server/models.py
+++ server/models.py
@@ -1,33 +1,44 @@
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy import DateTime
-from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import DateTime, ForeignKey
 import datetime
 
 from config import db
 
-# Models go here!
-
-class Product(db.Model):
-    __tablename__ = 'products'
+class Supplier(db.Model):
+    __tablename__ = 'suppliers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    description  = db.Column(db.String)
-    price = db.Column(db.String)
-    supplier_id = db.Column(db.Integer ForeignKey('suppliers.id'))
+    description = db.Column(db.String)
     date = db.Column(DateTime, default=datetime.datetime.utcnow)
 
     products = db.relationship('Product', back_populates='supplier')
 
     def __repr__(self):
-        return f'<Products {self.id}, {self.name}, {self.description}, {self.price}, {self.supplier_id}, {self.date}>'
+        return f'<Supplier {self.id}, {self.name}, {self.description}, {self.date}>'
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    price = db.Column(db.String)
+    supplier_id = db.Column(db.Integer, ForeignKey('suppliers.id'))
+    date = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+    supplier = db.relationship('Supplier', back_populates='products')
+    transactions = db.relationship('Transaction', back_populates='product')
+
+    def __repr__(self):
+        return f'<Product {self.id}, {self.name}, {self.description}, {self.price}, {self.supplier_id}, {self.date}>'
+
 class Transaction(db.Model):
     __tablename__ = 'transactions'
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer)
-    products_id = db.Column(db.Integer ForeignKey('products.id'))
+    product_id = db.Column(db.Integer, ForeignKey('products.id'))
     created_at = db.Column(DateTime, default=datetime.datetime.utcnow)
 
-    product = db.relationship('Product', back_populates = 'transactions')
+    product = db.relationship('Product', back_populates='transactions')
 
     def __repr__(self):
-        return f'<Transactions {self.id}, {self.quantity}, {self.products_id}, {self.created_at}>'
+        return f'<Transaction {self.id}, {self.quantity}, {self.product_id}, {self.created_at}>'
