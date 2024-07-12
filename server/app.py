55ab5c4e0ff7d5104e0178e6f0cd188a842fc208
kevinke3 <kevinke386@gmail.com>
2024-07-12 12:32:55 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 db.init_app(app)
 
-from models import Supplier, Product, Transaction,Order
+from models import Supplier, Product, Transaction, Order
 
 @app.route('/supplier', methods=['GET'])
 def get_suppliers():
